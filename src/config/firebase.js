e20062074b11182a61cc805ede5327ac2ef9d484
Dwi Agustianto <dwi.agustianto@ralali.com>
2019-12-12 10:01:00 +0700

    update firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,11 +2,12 @@ import * as firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBT67-cThnTNj9V6LNhyPhWU1lf1l4VCK4",
-  authDomain: "qimol-ralali.firebaseapp.com",
-  databaseURL: "https://qimol-ralali.firebaseio.com",
-  projectId: "qimol-ralali",
-  appId: "1:352043315675:web:ecbf291dd1d8f8be"
+  apiKey: "AIzaSyAk2yx6w-BAe54tkbY9ga9cmWTQ8Mc_jvQ",
+  authDomain: "ralali-unsung-hero.firebaseapp.com",
+  databaseURL: "https://ralali-unsung-hero.firebaseio.com",
+  projectId: "ralali-unsung-hero",
+  storageBucket: "ralali-unsung-hero.appspot.com",
+  appId: "1:124787742881:web:b0a59cfe1638cdff6638e0"
 }
 
 firebase.initializeApp(firebaseConfig)
